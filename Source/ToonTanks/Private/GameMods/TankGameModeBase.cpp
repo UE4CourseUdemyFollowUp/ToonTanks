3b58294c1b1fa093c3064275390101a5cb0fbf62
Eduard Korogodov <eduard.korogodov@gmail.com>
2021-04-10 23:23:28 +0300

    Add GameMode

---
+++ Source/ToonTanks/Private/GameMods/TankGameModeBase.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMods/TankGameModeBase.h"
+
+void ATankGameModeBase::BeginPlay()
+{
+}
+
+void ATankGameModeBase::HandleGameStart()
+{
+}
+
+void ATankGameModeBase::HandleGameOver(bool IsWin)
+{
+}
+
+void ATankGameModeBase::ActorDied(AActor* Actor)
+{
+}
+
